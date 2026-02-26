10cefd4a995119c248ae94995f780b55471e0d88
bayertom <bayertom@users.noreply.github.com>
2026-02-26 14:42:48 +0100

    cv1

--- Cv1/draw.py
+++ Cv1/draw.py
@@ -6,7 +6,40 @@ class Draw(QWidget):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.pol = QPolygonF()
-        self.q = QPointF(100, 100)
-        self.add_vertex = True
+        self.__pol = QPolygonF()
+        self.__q = QPointF(100, 100)
+        self.__add_vertex = True
+        
 
+    def mousePressEvent(self, e):
+        #Get cursor coordinates 
+        x = e.position().x()
+        y = e.position().y()
+        
+        #Create new point
+        p = QPointF(x,y)
+        
+        #Add P to polygon
+        self.__pol.append(p)
+        
+        #Repaint
+        self.repaint()
+        
+
+    def paintEvent(self, e):
+        #Draw situation
+        qp = QPainter(self)
+        
+        #Start draw
+        qp.begin(self)
+        
+        #Set attributes, polygon
+        qp.setPen(Qt.GlobalColor.black)
+        qp.setBrush(Qt.GlobalColor.yellow)
+        
+        #Draw polygon
+        qp.drawPolygon(self.__pol)
+        
+        #End draw
+        qp.end()
+        
