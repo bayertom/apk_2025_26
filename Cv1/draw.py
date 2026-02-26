e4c226621ddade733c32661bc103a785648ad0cf
bayertom <bayertom@users.noreply.github.com>
2026-02-26 12:58:07 +0100

    cv3

--- Cv1/draw.py
+++ Cv1/draw.py
@@ -6,7 +6,7 @@ class Draw(QWidget):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-	self.pol = QPolygonF()
+        self.pol = QPolygonF()
         self.q = QPointF(100, 100)
         self.add_vertex = True
 
